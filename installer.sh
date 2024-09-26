d4fffd95b96e408c8a99bc961bdb6fcc01b60a60
Bradin Rohde <152743324+BrohdeXC@users.noreply.github.com>
2024-09-26 01:16:35 -0600

    Update installer.sh
    Cleans up files better

--- installer.sh
+++ installer.sh
@@ -2,5 +2,6 @@
 
 git clone https://github.com/BrohdeXC/1337ness.git
 cd 1337ness && chmod +x 1337NESS LeetSpeak.py
-rm LICENSE README.md
+rm LICENSE README.md installer.sh
 sudo mv ../1337ness/* /usr/local/bin/ && cd .. && rm -rf 1337ness/
+rm installer.sh
